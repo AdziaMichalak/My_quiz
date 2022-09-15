11d37b95438e24a2c0d8be08e597c52eba3b18d5
Magdalena Michalak <mbawolik80@gmail.com>
2022-09-15 19:18:59 +0200

    Quiz

---
+++ My_quiz/quiz/tests/test_get_me_question.py
@@ -0,0 +1,9 @@
+import pytest
+from quiz.client import get_me_question
+from pytrivia import Trivia
+
+def test_get_me_question():
+    trivia_client = Trivia(True)
+    assert get_me_question
+
+
---
+++ My_quiz/quiz/tests/test_trivia.py
@@ -0,0 +1,9 @@
+import pytest
+
+from quiz.client import get_me_question
+
+
+
+def test_trivia():
+    question = get_me_question()
+    assert question
